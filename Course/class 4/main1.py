d9e42bf3c55e264b4f1f1a7139e5c5339bf17778
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-04-28 13:22:02 +0500

    example 2

--- Course/class 4/main1.py
+++ Course/class 4/main1.py
@@ -1,9 +1,21 @@
-print("My name is XYZ")
-fine = True
-if fine == True:
-    print("I am fine")
-    print("I will go school")
-else:
-    print("I am not fine")
+# print("My name is XYZ")
+# fine = True
+# if fine == True:
+#     print("I am fine")
+#     print("I will go school")
+# else:
+#     print("I am not fine")
+
+# print("End")
+
+
+# Example 2
+
+print("Start")
+i = 0
+
+while i < 10 :
+    print(i+1)
+    i+=1
 
 print("End")
