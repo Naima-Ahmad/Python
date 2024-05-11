3fd9130d82146e2d7399803ca6fc2d58cc02ca6e
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-05-11 05:53:36 +0500

    example 1

---
+++ practice/class 5/main.py
@@ -0,0 +1,9 @@
+# Example 1
+
+# studentName1 = input("Please enter your name: ")
+# studentName2 = input("Please enter your name: ")
+# studentName3 = input("Please enter your name: ")
+# studentName4 = input("Please enter your name: ")
+# studentName5 = input("Please enter your name: ")
+
+# Exampel 2
